f00fc1b71366b51254f45b64c5c16f776de9182c
chat1anin <spaceshuffle@icloud.com>
2025-12-11 05:35:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ ConversationHandler
 
 # Конфигурация
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_USER_ID = os.getenv("ADMIN_ID")  # Замените на ваш Telegram ID
+BOT_TOKEN = os.getenv(“BOT_TOKEN”)
+ADMIN_USER_ID = int(os.getenv(“ADMIN_ID”))
 DB_NAME = “audio_bot.db”
 
 # Состояния для ConversationHandler
